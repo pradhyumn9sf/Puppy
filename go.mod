568aa81f59077f704ca072635442f43c31499930
“Pradhyumn <prathore@splunk.com>
2025-04-11 11:53:39 +0530

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pradhyumn9sf/Puppy
 
 go 1.22.4
+
+require github.com/pradhyumn9sf/Dog v0.0.0-20250411062047-f443c08bcedd // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/pradhyumn9sf/Dog"
+)
+
 func Bark() string {
 	return "woof"
 }
 
 func Barks() string {
 	return "woof woof woof"
+}
+
+func BigBark() string {
+	return dog.WhenGrowUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrowUp(Barks())
 }
